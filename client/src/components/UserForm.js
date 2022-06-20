7042b222094bd2081f8abe4e6eb6431388b0e4ca
maorte <maor884@gmail.com>
2022-06-20 17:12:36 +0300

    some package upgrades

--- client/src/components/UserForm.js
+++ client/src/components/UserForm.js
@@ -1,159 +1,164 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+// import { makeStyles } from '@material-ui/core';
+import { makeStyles } from '@mui/styles';
+import { createTheme } from '@mui/material/styles';
+// import TextField from '@material-ui/core/TextField';
+import TextField from '@mui/material/TextField';
+// import Button from '@material-ui/core/Button';
+import Button from '@mui/material/Button';
 import { useForm, Controller } from 'react-hook-form';
 
+const theme = createTheme();
 const useStyles = makeStyles((theme) => ({
-	root: {
-		display: 'flex',
-		flexDirection: 'column',
-		justifyContent: 'center',
-		alignItems: 'center',
-		padding: theme.spacing(2),
+   root: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: theme.spacing(2),
 
-		'& .MuiTextField-root': {
-			margin: theme.spacing(1),
-			width: '300px',
-		},
-		'& .MuiButtonBase-root': {
-			margin: theme.spacing(2),
-		},
-	},
+      '& .MuiTextField-root': {
+         margin: theme.spacing(1),
+         width: '300px',
+      },
+      '& .MuiButtonBase-root': {
+         margin: theme.spacing(2),
+      },
+   },
 }));
 
 const israeliPhoneRegex = /^\+?(972|0)(-)?0?(([23489]{1}\d{7})|[5]{1}\d{8})$/;
 const ipAddressRegex = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}/;
 
 const UserForm = ({ handleFormSubmit, handleClose }) => {
-	const classes = useStyles();
-	const { handleSubmit, control } = useForm();
+   const classes = useStyles();
+   const { handleSubmit, control } = useForm();
 
-	const onSubmitForm = (data) => {
-		handleClose();
-		handleFormSubmit(data);
-	};
-	return (
-		<form className={classes.root} onSubmit={handleSubmit(onSubmitForm)}>
-			<Controller
-				name="Name"
-				control={control}
-				defaultValue=""
-				render={({ field: { onChange, value }, fieldState: { error } }) => (
-					<TextField
-						label="Name"
-						variant="outlined"
-						value={value}
-						onChange={onChange}
-						error={!!error}
-						helperText={error ? error.message : null}
-						size="medium"
-					/>
-				)}
-				rules={{ required: 'Name required' }}
-			/>
-			<Controller
-				name="Email"
-				control={control}
-				defaultValue=""
-				render={({ field: { onChange, value }, fieldState: { error } }) => (
-					<TextField
-						label="Email"
-						variant="outlined"
-						value={value}
-						onChange={onChange}
-						error={!!error}
-						helperText={error ? error.message : null}
-						type="email"
-						size="medium"
-					/>
-				)}
-				rules={{
-					required: 'Email required',
-					pattern: {
-						value: /\S+@\S+\.\S+/,
-						message: 'Invalid email',
-					},
-				}}
-			/>
-			<Controller
-				name="ID"
-				control={control}
-				defaultValue=""
-				render={({ field: { onChange, value }, fieldState: { error } }) => (
-					<TextField
-						label="ID"
-						variant="outlined"
-						value={value}
-						onChange={onChange}
-						error={!!error}
-						helperText={error ? error.message : null}
-						type="number"
-						size="medium"
-					/>
-				)}
-				rules={{
-					required: 'ID required',
-					pattern: { value: /^\d{9}$/, message: 'ID should contain 9 digits' },
-				}}
-			/>
-			<Controller
-				name="Phone"
-				control={control}
-				defaultValue=""
-				render={({ field: { onChange, value }, fieldState: { error } }) => (
-					<TextField
-						label="Phone"
-						variant="outlined"
-						value={value}
-						onChange={onChange}
-						error={!!error}
-						helperText={error ? error.message : null}
-						type="tel"
-						size="medium"
-					/>
-				)}
-				rules={{
-					required: 'Phone required',
-					pattern: {
-						value: israeliPhoneRegex,
-						message: 'Invalid Phone Number',
-					},
-				}}
-			/>
-			<Controller
-				name="IP"
-				control={control}
-				defaultValue=""
-				render={({ field: { onChange, value }, fieldState: { error } }) => (
-					<TextField
-						label="IP"
-						variant="outlined"
-						value={value}
-						onChange={onChange}
-						error={!!error}
-						helperText={error ? error.message : null}
-						size="medium"
-					/>
-				)}
-				rules={{
-					required: 'IP required',
-					pattern: {
-						value: ipAddressRegex,
-						message: 'Invalid IP address',
-					},
-				}}
-			/>
-			<div>
-				<Button type="submit" variant="contained" color="primary">
-					add user
-				</Button>
-				<Button onClick={handleClose} variant="contained" color="primary">
-					close
-				</Button>
-			</div>
-		</form>
-	);
+   const onSubmitForm = (data) => {
+      handleClose();
+      handleFormSubmit(data);
+   };
+   return (
+      <form className={classes.root} onSubmit={handleSubmit(onSubmitForm)}>
+         <Controller
+            name="Name"
+            control={control}
+            defaultValue=""
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
+               <TextField
+                  label="Name"
+                  variant="outlined"
+                  value={value}
+                  onChange={onChange}
+                  error={!!error}
+                  helperText={error ? error.message : null}
+                  size="medium"
+               />
+            )}
+            rules={{ required: 'Name required' }}
+         />
+         <Controller
+            name="Email"
+            control={control}
+            defaultValue=""
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
+               <TextField
+                  label="Email"
+                  variant="outlined"
+                  value={value}
+                  onChange={onChange}
+                  error={!!error}
+                  helperText={error ? error.message : null}
+                  type="email"
+                  size="medium"
+               />
+            )}
+            rules={{
+               required: 'Email required',
+               pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: 'Invalid email',
+               },
+            }}
+         />
+         <Controller
+            name="ID"
+            control={control}
+            defaultValue=""
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
+               <TextField
+                  label="ID"
+                  variant="outlined"
+                  value={value}
+                  onChange={onChange}
+                  error={!!error}
+                  helperText={error ? error.message : null}
+                  type="number"
+                  size="medium"
+               />
+            )}
+            rules={{
+               required: 'ID required',
+               pattern: { value: /^\d{9}$/, message: 'ID should contain 9 digits' },
+            }}
+         />
+         <Controller
+            name="Phone"
+            control={control}
+            defaultValue=""
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
+               <TextField
+                  label="Phone"
+                  variant="outlined"
+                  value={value}
+                  onChange={onChange}
+                  error={!!error}
+                  helperText={error ? error.message : null}
+                  type="tel"
+                  size="medium"
+               />
+            )}
+            rules={{
+               required: 'Phone required',
+               pattern: {
+                  value: israeliPhoneRegex,
+                  message: 'Invalid Phone Number',
+               },
+            }}
+         />
+         <Controller
+            name="IP"
+            control={control}
+            defaultValue=""
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
+               <TextField
+                  label="IP"
+                  variant="outlined"
+                  value={value}
+                  onChange={onChange}
+                  error={!!error}
+                  helperText={error ? error.message : null}
+                  size="medium"
+               />
+            )}
+            rules={{
+               required: 'IP required',
+               pattern: {
+                  value: ipAddressRegex,
+                  message: 'Invalid IP address',
+               },
+            }}
+         />
+         <div>
+            <Button type="submit" variant="contained" color="primary">
+               add user
+            </Button>
+            <Button onClick={handleClose} variant="contained" color="primary">
+               close
+            </Button>
+         </div>
+      </form>
+   );
 };
 
 export default UserForm;
--- client/src/components/UserModal.js
+++ client/src/components/UserModal.js
@@ -3,40 +3,42 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-
 import UserForm from './UserForm';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
+
 const style = {
-	position: 'absolute',
-	top: '40%',
-	left: '50%',
-	transform: 'translate(-50%, -50%)',
-	width: 300,
-	bgcolor: 'background.paper',
-	border: '2px solid #000',
-	boxShadow: 24,
-	p: 4,
+   position: 'absolute',
+   top: '40%',
+   left: '50%',
+   transform: 'translate(-50%, -50%)',
+   width: 300,
+   bgcolor: 'background.paper',
+   border: '2px solid #000',
+   boxShadow: 24,
+   p: 4,
 };
 export default function UserModal({ handleFormSubmit }) {
-	const [open, setOpen] = useState(false);
-	const handleOpen = () => setOpen(true);
-	const handleClose = () => setOpen(false);
+   const [open, setOpen] = useState(false);
+   const handleOpen = () => setOpen(true);
+   const handleClose = () => setOpen(false);
 
-	return (
-		<>
-			<Button onClick={handleOpen} variant="contained">
-				add user
-			</Button>
-			<Modal open={open} onClose={handleClose}>
-				<Box sx={style}>
-					<Typography variant="h6" component="h2">
-						Add User
-					</Typography>
-					<UserForm
-						handleFormSubmit={handleFormSubmit}
-						handleClose={handleClose}
-					/>
-				</Box>
-			</Modal>
-		</>
-	);
+   return (
+      <>
+         <Button onClick={handleOpen} variant="contained">
+            add user
+         </Button>
+         <Modal open={open} onClose={handleClose}>
+            <Box sx={style}>
+               <Typography variant="h6" component="h2">
+                  Add User
+               </Typography>
+               <ThemeProvider theme={theme}>
+                  <UserForm handleFormSubmit={handleFormSubmit} handleClose={handleClose} />
+               </ThemeProvider>
+            </Box>
+         </Modal>
+      </>
+   );
 }
